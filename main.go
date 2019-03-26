7b22fe4e0b54e185783da66c57eef71bcc26de46
赵威杰 <1060656096@qq.com>
2019-03-26 19:53:34 +0800

    caa: 增加日志分割

--- main.go
+++ main.go
@@ -5,11 +5,28 @@ import (
 	"gapp/routers"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/lestrrat/go-file-rotatelogs"
+	"log"
+	"time"
 )
 
 func main() {
 	godotenv.Load(".env")
-	engine := gin.New()
+
+	logf, err := rotatelogs.New(
+		"var/log/gin.%Y%m%d.%H%M.log",
+		rotatelogs.WithLinkName("/var/log/access_log"),
+		rotatelogs.WithMaxAge(24 * time.Hour),
+		rotatelogs.WithRotationTime(time.Hour),
+	)
+	if err != nil {
+		log.Printf("failed to create rotatelogs: %s", err)
+		return
+	}
+
+
+	gin.DefaultWriter = logf
+	engine := gin.Default()
 	engine.LoadHTMLGlob("resources/themes/***/***/*")
 	// 初始化路由
 	routers.InitRouter(engine)
